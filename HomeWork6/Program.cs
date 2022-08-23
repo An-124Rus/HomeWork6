d1933fc7ccfeee595171ae94de7d4d9b9c66fe71
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-08-23 13:53:06 +0300

    HomeWork6

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int gold;
+        int crystal;
+        int price = 10;
+        int totalCost;        
+
+        Console.Write("Введите количество золота: ");
+        gold = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Стоимость кристалла - " + price + ". Сколько красталлов необходимо: ");
+        crystal = Convert.ToInt32(Console.ReadLine());
+
+        totalCost = price * crystal;
+        gold -= totalCost;
+
+        Console.WriteLine($"Вы купили {crystal} кристаллов и у вас осталось {gold} золота");
+    }
+}
