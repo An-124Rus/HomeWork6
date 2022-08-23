692e2b51c5cbe469c947f9c39034b29d0272f659
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-08-23 14:59:45 +0300

    Update Program.cs

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -4,15 +4,15 @@
     {
         int gold;
         int crystal;
-        int price = 10;
+        int crystalPrice = 10;
         int totalCost;        
 
         Console.Write("Введите количество золота: ");
         gold = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Стоимость кристалла - " + price + ". Сколько красталлов необходимо: ");
+        Console.Write("Стоимость кристалла - " + crystalPrice + ". Сколько красталлов необходимо: ");
         crystal = Convert.ToInt32(Console.ReadLine());
 
-        totalCost = price * crystal;
+        totalCost = crystalPrice * crystal;
         gold -= totalCost;
 
         Console.WriteLine($"Вы купили {crystal} кристаллов и у вас осталось {gold} золота");
